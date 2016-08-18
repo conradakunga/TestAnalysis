53fefb679a99b616d801e7f62e5aa0a0926d198c
Conrad Akunga <cakunga@innova.co.ke>
2016-08-18 21:14:17 +0300

    Initial commit

---
+++ NUnitTests/Class1.cs
@@ -0,0 +1,134 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    [Category("NUnit")]
+    public class Class0
+    {
+
+        [Test]
+        public void Test0()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Category("NUnit")]
+    public class Class1
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Category("NUnit")]
+    public class Class2
+    {
+
+        [Test]
+        public void Test2()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Category("NUnit")]
+    public class Class3
+    {
+
+        [Test]
+        public void Test3()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Category("NUnit")]
+    public class Class4
+    {
+
+        [Test]
+        public void Test4()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Category("NUnit")]
+    public class Class5
+    {
+
+        [Test]
+        public void Test5()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Category("NUnit")]
+    public class Class6
+    {
+
+        [Test]
+        public void Test6()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Category("NUnit")]
+    public class Class7
+    {
+
+        [Test]
+        public void Test7()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Category("NUnit")]
+    public class Class8
+    {
+
+        [Test]
+        public void Test8()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Category("NUnit")]
+    public class Class9
+    {
+
+        [Test]
+        public void Test9()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+
+
+}
---
+++ xUnitTests/Class1.cs
@@ -0,0 +1,134 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitFacts
+{
+    [Trait("Category", "xUnit")]
+    public class Class0
+    {
+
+        [Fact]
+        public void Test0()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Trait("Category", "xUnit")]
+    public class Class1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Trait("Category", "xUnit")]
+    public class Class2
+    {
+
+        [Fact]
+        public void Test2()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Trait("Category", "xUnit")]
+    public class Class3
+    {
+
+        [Fact]
+        public void Test3()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Trait("Category", "xUnit")]
+    public class Class4
+    {
+
+        [Fact]
+        public void Test4()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Trait("Category", "xUnit")]
+    public class Class5
+    {
+
+        [Fact]
+        public void Test5()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Trait("Category", "xUnit")]
+    public class Class6
+    {
+
+        [Fact]
+        public void Test6()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Trait("Category", "xUnit")]
+    public class Class7
+    {
+
+        [Fact]
+        public void Test7()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Trait("Category", "xUnit")]
+    public class Class8
+    {
+
+        [Fact]
+        public void Test8()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+    [Trait("Category", "xUnit")]
+    public class Class9
+    {
+
+        [Fact]
+        public void Test9()
+        {
+            Thread.Sleep(1000);
+            true.Should().BeTrue();
+        }
+    }
+
+
+
+}
